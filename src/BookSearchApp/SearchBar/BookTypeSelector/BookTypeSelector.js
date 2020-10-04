b92409a04a7feabfb7e86303ad8800759dc662c3
Sebastian Holmes <SKerlec7693@gmail.com>
2020-10-03 19:52:39 -0700

    relocated state and consolidated searchBar components

--- src/BookSearchApp/SearchBar/BookTypeSelector/BookTypeSelector.js
+++ src/BookSearchApp/SearchBar/BookTypeSelector/BookTypeSelector.js
@@ -7,7 +7,10 @@ class BookTypeSelector extends Component {
             <div className='BookTypeSelector'>
                 <form>
                     <label htmlFor='BookTypeSelector__options'>Book Type: </label>
-                    <select id='BookTypeSelector__options' name='BookTypeSelector__options' onChange={e => this.props.bookTypeSelection(e.target.value)}>
+                    <select 
+                        id='BookTypeSelector__options' 
+                        name='BookTypeSelector__options' 
+                        onChange={e => this.props.bookTypeSelection(e.target.value)}>
                         <option value='all'>All</option>
                         <option value='full'>Full</option>
                         <option value='partial'>Partial</option>
